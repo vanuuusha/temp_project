33566d1e1944bd76402f264beea5e3ecbbd05427
ivan.korotchenya@gmail.com <ivan.korotchenya@gmail.com>
2024-03-30 20:07:30 +0300

    init

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ def gen_random_sequence(length):
     random_sequence = ''.join(random.choice(string.ascii_letters) for _ in range(length))
     return random_sequence
 
-def complete_first_step(page):
+def complete_first_step(page, now_file):
     page.check('input[type="checkbox"][name="terms"]')
     page.fill('input[name="mobile"]', '027'+str(random.randint(1000000, 9999999)))
     page.press('input[name="mobile"]', 'Enter')
     page.select_option('select[name="shapes_product"]', value='Other')
     page.select_option('select[name="retailer"]', value='Countdown/Woolworths')
     page.select_option('select[name="online"]', value='No')
-    now_file = random.choice([x for x in os.listdir('./photos') if x.endswith('.jpg')])
     submit_file(page, now_file)
 
 
@@ -30,22 +29,22 @@ def submit_file(page, now_file):
     page.click('css=button[type="button"]')
     time.sleep(2)
     page.set_input_files('#receipt_upload', f'/home/vanusha/temp_project/photos/{now_file}')
-    time.sleep(10)
+    time.sleep(5)
     page.click('css=button[type="submit"]')
 
 
 def complete_second_step(page, mail):
-    page.fill('input[name="first_name"]', gen_random_sequence(random.randint(4, 10)))
+    page.fill('input[name="first_name"]', random.choice(names))
     time.sleep(2)
-    page.fill('input[name="last_name"]', gen_random_sequence(random.randint(4, 10)))
+    page.fill('input[name="last_name"]', random.choice(surnames))
     time.sleep(2)
     page.fill('input[name="email"]', mail)
     time.sleep(1)
     page.fill('input[name="confirm_email"]', mail)
     time.sleep(1)
-    page.fill('input[name="address_line_1"]', gen_random_sequence(random.randint(4, 10)))
+    page.fill('input[name="address_line_1"]', random.choice(addresses))
     time.sleep(1)
-    page.fill('input[name="suburb"]', gen_random_sequence(random.randint(4, 10)))
+    page.fill('input[name="suburb"]', random.choice(cities))
     time.sleep(1)
     page.select_option('select[name="state"]', value='Gisborne')
     time.sleep(1)
@@ -58,17 +57,24 @@ def complete_second_step(page, mail):
     page.click('css=button[type="submit"]')
 
 def runner(mail, my_name):
+    proxy = random.choice(proxies)
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=headless)
+        browser = p.chromium.launch(headless=headless, proxy={
+            "server": f'{proxy["host"]}:{proxy["port"]}',
+            "username": proxy['username'],
+            "password": proxy['password']
+        })
         page = browser.new_page()
 
         page.goto('https://www.shapeswin.com/nz')
         logger.info(f'Процесс {my_name}: Перешел на сайт')
         try:
-            complete_first_step(page)
+            now_file = random.choice([x for x in os.listdir('./photos') if x.endswith('.jpg')])
+            complete_first_step(page, now_file)
         except TimeoutError:
             logger.warning(f'Процесс {my_name}: Ошибка при заполнение форму, попробую еще раз не удаляя почту')
         else:
+            os.remove(f'./photos/{now_file}')
             expected_text = 'CONTACT DETAILS'
             try:
                 time.sleep(15)
@@ -79,17 +85,17 @@ def runner(mail, my_name):
                 logger.info(f'Процесс {my_name}: Завершил первый этап')
                 complete_second_step(page, mail)
                 logger.info(f'Процесс {my_name}: Завершил второй этап')
-                expected_text = 'CONGRATS'
-                try:
-                    time.sleep(15)
-                    page.wait_for_function(f'document.querySelector(".heading__winner_content").innerText.includes("{expected_text}")')
-                except TimeoutError:
-                    logger.error(f'Процесс {my_name}: Удаляю почту победа не обнаружена')
-                    remove_processed_emails(mail)
-                else:
-                    remove_processed_emails(mail)
-                    add_email(mail)
-                    logger.info(f'Процесс {my_name}: Все прошло успешно, почта {mail}')
+                remove_processed_emails(mail)
+                add_email(mail)
+                logger.info(f'Процесс {my_name}: Все прошло успешно, почта {mail}')
+                time.sleep(100)
+                # expected_text = 'CONGRATS'
+                # try:
+                #     time.sleep(15)
+                # except TimeoutError:
+                #     logger.error(f'Процесс {my_name}: Удаляю почту победа не обнаружена')
+                #     remove_processed_emails(mail)
+                # else:
 
 
 def add_email(email_to_remove):
@@ -126,6 +132,22 @@ def complete_all(lines):
 
 if __name__ == '__main__':
     lines = True
+    with open('./proxy.txt', 'r') as file:
+        proxies = file.readlines()
+        proxies = [i[:-1].split(':') for i in proxies]
+        proxies = [{'host': i[0], 'port': i[1], 'username': i[2], 'password': i[3]} for i in proxies]
+    with open('./address.txt', 'r') as file:
+        addresses = file.readlines()
+        addresses = [i[:-1] for i in addresses]
+    with open('./names.txt', 'r') as file:
+        names = file.readlines()
+        names = [i[:-1] for i in names]
+    with open('cities.txt', 'r') as file:
+        cities = file.readlines()
+        cities = [i[:-1] for i in cities]
+    with open('./Surnames.txt', 'r') as file:
+        surnames = file.readlines()
+        surnames = [i[:-1] for i in surnames]
     while lines:
         with open('./input_file.txt', 'r') as file:
             lines = file.readlines()
