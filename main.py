bb6fc3c2d74bb0d3f0acaaa13f90e6af16f9188d
ivan.korotchenya@gmail.com <ivan.korotchenya@gmail.com>
2024-03-30 16:04:19 +0300

    init

---
+++ main.py
@@ -0,0 +1,132 @@
+import os
+import time
+from playwright.sync_api import sync_playwright, TimeoutError
+import random
+import string
+from loguru import logger
+import multiprocessing
+from settings import num_threads, headless
+
+def gen_random_sequence(length):
+    random_sequence = ''.join(random.choice(string.ascii_letters) for _ in range(length))
+    return random_sequence
+
+def complete_first_step(page):
+    page.check('input[type="checkbox"][name="terms"]')
+    page.fill('input[name="mobile"]', '027'+str(random.randint(1000000, 9999999)))
+    page.press('input[name="mobile"]', 'Enter')
+    page.select_option('select[name="shapes_product"]', value='Other')
+    page.select_option('select[name="retailer"]', value='Countdown/Woolworths')
+    page.select_option('select[name="online"]', value='No')
+    now_file = random.choice([x for x in os.listdir('./photos') if x.endswith('.jpg')])
+    submit_file(page, now_file)
+
+
+def submit_file(page, now_file):
+    page.focus('#receipt_upload')
+    page.click('#receipt_upload')
+    page.set_input_files('#receipt_upload', f'/home/vanusha/temp_project/photos/{now_file}')
+    time.sleep(5)
+    page.click('css=button[type="button"]')
+    time.sleep(2)
+    page.set_input_files('#receipt_upload', f'/home/vanusha/temp_project/photos/{now_file}')
+    time.sleep(10)
+    page.click('css=button[type="submit"]')
+
+
+def complete_second_step(page, mail):
+    page.fill('input[name="first_name"]', gen_random_sequence(random.randint(4, 10)))
+    time.sleep(2)
+    page.fill('input[name="last_name"]', gen_random_sequence(random.randint(4, 10)))
+    time.sleep(2)
+    page.fill('input[name="email"]', mail)
+    time.sleep(1)
+    page.fill('input[name="confirm_email"]', mail)
+    time.sleep(1)
+    page.fill('input[name="address_line_1"]', gen_random_sequence(random.randint(4, 10)))
+    time.sleep(1)
+    page.fill('input[name="suburb"]', gen_random_sequence(random.randint(4, 10)))
+    time.sleep(1)
+    page.select_option('select[name="state"]', value='Gisborne')
+    time.sleep(1)
+    page.fill('input[name="postcode"]', str(random.randint(1000, 9999)))
+    time.sleep(1)
+    page.check('input[type="checkbox"][name="over_eighteen"]')
+    page.check('input[type="checkbox"][name="marketing"]')
+    page.check('input[type="checkbox"][name="privacy"]')
+    time.sleep(3)
+    page.click('css=button[type="submit"]')
+
+def runner(mail, my_name):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=headless)
+        page = browser.new_page()
+
+        page.goto('https://www.shapeswin.com/nz')
+        logger.info(f'Процесс {my_name}: Перешел на сайт')
+        try:
+            complete_first_step(page)
+        except TimeoutError:
+            logger.warning(f'Процесс {my_name}: Ошибка при заполнение форму, попробую еще раз не удаляя почту')
+        else:
+            expected_text = 'CONTACT DETAILS'
+            try:
+                time.sleep(15)
+                page.wait_for_function(f'document.querySelector("h1").innerText.includes("{expected_text}")', timeout=5000)
+            except TimeoutError:
+                logger.warning(f'Процесс {my_name}: Ошибка при заполнение форму, попробую еще раз не удаляя почту')
+            else:
+                logger.info(f'Процесс {my_name}: Завершил первый этап')
+                complete_second_step(page, mail)
+                logger.info(f'Процесс {my_name}: Завершил второй этап')
+                expected_text = 'CONGRATS'
+                try:
+                    time.sleep(15)
+                    page.wait_for_function(f'document.querySelector(".heading__winner_content").innerText.includes("{expected_text}")')
+                except TimeoutError:
+                    logger.error(f'Процесс {my_name}: Удаляю почту победа не обнаружена')
+                    remove_processed_emails(mail)
+                else:
+                    remove_processed_emails(mail)
+                    add_email(mail)
+                    logger.info(f'Процесс {my_name}: Все прошло успешно, почта {mail}')
+
+
+def add_email(email_to_remove):
+    with open('./output_file.txt', 'a+') as output_file:
+        output_file.write('\n'+email_to_remove)
+
+
+def remove_processed_emails(email_to_remove):
+    with open('./input_file.txt', 'r') as input_file_read:
+        lines = input_file_read.readlines()
+
+    with open('./input_file.txt', 'w') as input_file_write:
+        for line in lines:
+            email = line.strip().split(':')[0]
+            if email not in email_to_remove:
+                input_file_write.write(line)
+
+
+def complete_all(lines):
+    processes = []
+    for line in lines:
+        mail = line.strip()
+        process = multiprocessing.Process(target=runner, args=(mail, len(processes)))
+        processes.append(process)
+        process.start()
+
+        if len(processes) >= num_threads:
+            for process in processes:
+                process.join()
+            processes = []
+    for process in processes:
+        process.join()
+
+
+if __name__ == '__main__':
+    lines = True
+    while lines:
+        with open('./input_file.txt', 'r') as file:
+            lines = file.readlines()
+        complete_all(lines)
---
+++ settings.py
@@ -0,0 +1,2 @@
+num_threads = 1
+headless = False
